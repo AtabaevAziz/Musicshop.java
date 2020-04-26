1fd9bc138d4df6061487a5713ea706b1d68adf07
aziz <19atabaev.aziz@gmail.com>
2020-04-26 13:25:10 +0500

    Sending e-mail with order Implicit Intent.

--- app/src/main/java/com/example/musicshop/OrderActivity.java
+++ app/src/main/java/com/example/musicshop/OrderActivity.java
@@ -3,10 +3,16 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    String[] addresses = {"rano.jabbarova70@gmail.com"};
+    String subject = "Order from Music Shop";
+    String emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +29,23 @@ public class OrderActivity extends AppCompatActivity {
         String goodsName = reciverdOrderIntent.getStringExtra("goodsName");
         TextView orderTextView = findViewById(R.id.orderUserName);
 
-        orderTextView.setText("Customer's Name: " + userName
+        emailText = "Customer's Name: " + userName
                 + "\n" + "Subject: " + goodsName
                 + "\n" + "Quantity: " + quantity
                 + "\n" + "Price: " + price
-                + "\n" + "Order Price: " + orderPrice);
+                + "\n" + "Order Price: " + orderPrice;
+        orderTextView.setText(emailText);
+    }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL,  addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
+
